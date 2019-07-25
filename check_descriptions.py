c435fc484293e2b78a673517ab2063536c83e6e5
Aarni Koskela <akx@iki.fi>
2019-07-25 16:51:41 +0300

    Check that all .rst files have :description: blocks

---
+++ check_descriptions.py
@@ -0,0 +1,16 @@
+import glob
+import sys
+
+names_without_description_blocks = []
+for name in glob.glob('source/**/**.rst', recursive=True):
+    with open(name, 'r') as fh:
+        content = fh.read()
+    if ':description: ' not in content:
+        names_without_description_blocks.append(name)
+
+if names_without_description_blocks:
+    print('Files missing `:description:` blocks:\n', file=sys.stderr)
+    for name in sorted(names_without_description_blocks):
+        print(name)
+    print('', file=sys.stderr)
+    sys.exit(1)
