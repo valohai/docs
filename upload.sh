ae681bdd758a0cae3a5cdef40cc9df297391a1b3
ruksi <me@ruk.si>
2019-12-11 12:38:27 +0200

    Fix production builder to use directory builder

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xeuo pipefail
 
-aws s3 sync --delete --acl public-read build/html/ s3://docs.valohai.com/
+aws s3 sync --delete --acl public-read build/dirhtml/ s3://docs.valohai.com/
